2d81b9e7f17eb609f960ea5149119a07d50612bf
randomsilver <Vivienne.McGregor@lbi.com>
2013-02-07 19:02:09 +0000

    Added Dynamic pages from Chris Coyier's Ajax call code

---
+++ js/dynamicpage.js
@@ -0,0 +1,71 @@
+$(function() {
+
+    var newHash      = "",
+        $mainContent = $("#main-content"),
+        $pageWrap    = $("#page-wrap"),
+        baseHeight   = 0,
+        $el;
+        
+    $pageWrap.height($pageWrap.height());
+    baseHeight = $pageWrap.height() - $mainContent.height();
+    
+    $("nav").delegate("a", "click", function() {
+        window.location.hash = $(this).attr("href");
+        return false;
+    });
+
+    $(function() {
+
+         // load the content
+        function loadContent(href){
+            $mainContent
+                .find("#guts")
+                .fadeOut(200, function() {
+                    $mainContent.hide().load(href + " #guts", function() {
+                    $mainContent.fadeIn(200, function() {
+                        $pageWrap.animate({
+                            height: baseHeight + $mainContent.height() + "px"
+                        });
+                    });
+                    $("nav a").removeClass("current");
+                    console.log(href);
+                    $("nav a[href$="+href+"]").addClass("current");
+                });
+            });
+        }; 
+
+      if (Modernizr.history) {
+
+        // history is supported; do magical things
+
+        var newHash      = "",
+            $mainContent = $("#main-content"),
+            $pageWrap    = $("#page-wrap"),
+            baseHeight   = 0,
+            $el;
+
+
+        $pageWrap.height($pageWrap.height());
+        baseHeight = $pageWrap.height() - $mainContent.height();
+
+        // hijack the nav click event
+        $("nav").delegate("a", "click", function() {
+            _link = $(this).attr("href");
+            history.pushState(null, null, _link);
+            loadContent(_link);
+            return false;
+        }); 
+
+       
+
+          // change the url without a page refresh and add a history entry.
+        $(window).bind('popstate', function(){
+            _link = location.pathname.replace(/^.*[\\\/]/, ''); //get filename only
+            loadContent(_link);
+        }); 
+
+   } // history is not supported; nothing fancy here
+
+});
+
+});
